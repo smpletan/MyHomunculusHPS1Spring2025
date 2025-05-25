02d2069b7ddeb8ea540afa7bea706f9179076931
Nar <74840406+Narwhix@users.noreply.github.com>
2025-05-25 05:15:34 -0500

    Added Background Sprites, restructured UI, added mask for Background
    background is now in a subviewport with a shader, it is over the pattern, and under the OuterUI
    
    Also added a scrolling pattern texture alt in case we wanted Blue Background

--- Scripts/BackgroundUse.gd
+++ Scripts/BackgroundUse.gd
@@ -1,4 +1,4 @@
-extends Sprite2D
+extends AnimatedSprite2D
 
-func _process(delta: float) -> void:
-	position.x += (get_viewport().get_mouse_position().x/1.5*delta)-position.x
+#func _process(delta: float) -> void:
+	#position.x += (get_viewport().get_mouse_position().x/-1.5*delta)-position.x
