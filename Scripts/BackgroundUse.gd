eb08552dedf2d9d047a6d51c6b1fa1e8b19042a9
Nar <74840406+Narwhix@users.noreply.github.com>
2025-05-26 09:21:25 -0500

    Added Opening/Reset Animation + Audio
    The animation player is inside of the BackForeGround scene
    by default its set to "reset" which keeps the canvas layer hidden
    if you play the animation Opening, it will unhide the canvas layer with the AnimatedSprite2D and play the audio alongside it

--- Scripts/BackgroundUse.gd
+++ Scripts/BackgroundUse.gd
@@ -1,4 +1,4 @@
 extends AnimatedSprite2D
 
 #func _process(delta: float) -> void:
-	#position.x += (get_viewport().get_mouse_position().x/-1.5*delta)-position.x
+	#position += (get_viewport().get_mouse_position()/-1.5*delta)-position
