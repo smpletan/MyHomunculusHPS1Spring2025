ac9c425cff50cc5584ca03fa533b7ae5e08cfc24
smpletan <119008557+smpletan@users.noreply.github.com>
2025-05-28 21:19:11 -0400

    Implemented and updated meters
    Added functional meters that will update (via Dialogic signals) every time stats change and will fill/decrease accordingly. Also began work on a Glossary for general terms.

--- Scripts/gameplaymenu.gd
+++ Scripts/gameplaymenu.gd
@@ -11,3 +11,12 @@ func _on_dialogic_signal(argument:String):
 	if argument == "begin_cutscene":
 		Dialogic.paused = true
 		get_node("Background").get_node("%OpeningAnimationPlayer").play("OpeningAnimation")
+	elif argument == "update_stats":
+		$OuterUI/StatMeterVBox/LoveMeter.value = Dialogic.VAR.Stats.love
+		$OuterUI/StatMeterVBox/SmartsMeter.value = Dialogic.VAR.Stats.smarts
+		$OuterUI/StatMeterVBox/StrengthMeter.value = Dialogic.VAR.Stats.strength
+		$OuterUI/StatMeterVBox/SpeedMeter.value = Dialogic.VAR.Stats.speed
+		$OuterUI/StatMeterVBox/BraveryMeter.value = Dialogic.VAR.Stats.bravery
+		$OuterUI/StatMeterVBox/DiligenceMeter.value = Dialogic.VAR.Stats.diligence
+		$OuterUI/StatMeterVBox/MediaLitMeter.value = Dialogic.VAR.Stats.medialit
+		$OuterUI/StatMeterVBox/DrivingMeter.value = Dialogic.VAR.Stats.driving
