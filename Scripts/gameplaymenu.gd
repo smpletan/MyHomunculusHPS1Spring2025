8b330c328e34103dd1ac67d17caf5a194d8fe4ec
smpletan <119008557+smpletan@users.noreply.github.com>
2025-05-30 08:58:44 -0400

    Added activity animations, stats, and money
    Continued work on implementing the general game loop systems. Player can now do most tasks in the city with their corresponding animations playing.

--- Scripts/gameplaymenu.gd
+++ Scripts/gameplaymenu.gd
@@ -3,6 +3,7 @@ extends Node2D
 var anim
 
 func _ready():
+	$OuterUI/ActivityContainer.visible = false
 	Dialogic.signal_event.connect(_on_dialogic_signal)
 	get_node("Background").get_node("%StartFadeAnimationPlayer").play("LightsOffFade_OUT")
 
@@ -20,3 +21,30 @@ func _on_dialogic_signal(argument:String):
 		$OuterUI/StatMeterVBox/DiligenceMeter.value = Dialogic.VAR.Stats.diligence
 		$OuterUI/StatMeterVBox/MediaLitMeter.value = Dialogic.VAR.Stats.medialit
 		$OuterUI/StatMeterVBox/DrivingMeter.value = Dialogic.VAR.Stats.driving
+		$OuterUI/MoneyContainer/MoneyDisplay.text = str(int(Dialogic.VAR.cash))
+	elif argument == "city_transition":
+		pass
+	elif argument == "home_transition":
+		pass
+	elif argument == "lab_transition":
+		pass
+	elif argument == "act_workout":
+		$OuterUI/ActivityContainer.visible = true
+		$OuterUI/ActivityContainer/ActivityPlayer/ActivityAnimation.play("WorkoutBench")
+	elif argument == "act_running":
+		$OuterUI/ActivityContainer.visible = true
+		$OuterUI/ActivityContainer/ActivityPlayer/ActivityAnimation.play("RunningAround")
+	elif argument == "act_playground":
+		$OuterUI/ActivityContainer.visible = true
+		$OuterUI/ActivityContainer/ActivityPlayer/ActivityAnimation.play("PlaygroundSwing")
+	elif argument == "act_parttime":
+		$OuterUI/ActivityContainer.visible = true
+		$OuterUI/ActivityContainer/ActivityPlayer/ActivityAnimation.play("PartTimeJob")
+	elif argument == "act_reading":
+		$OuterUI/ActivityContainer.visible = true
+		$OuterUI/ActivityContainer/ActivityPlayer/ActivityAnimation.play("Reading")
+	elif argument == "act_watchcars":
+		$OuterUI/ActivityContainer.visible = true
+		$OuterUI/ActivityContainer/ActivityPlayer/ActivityAnimation.play("WatchingCars")
+	elif argument == "end_act":
+		$OuterUI/ActivityContainer.visible = false
