b960652c098f922ea7a9baa7ea816637d30f6b3e
Nar <74840406+Narwhix@users.noreply.github.com>
2025-06-16 00:47:01 -0500

    music in, still scuffed

--- opening_animation_player.gd
+++ opening_animation_player.gd
@@ -3,3 +3,5 @@ extends AnimationPlayer
 func _on_animation_finished(_OpeningAnimation):
 	print("Cutscene done")
 	Dialogic.paused = false
+	#get_parent().get_node("Menu").get_node("MainMusicPlayer").set_playing(true)
+	pass
