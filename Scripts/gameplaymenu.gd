046a9507af71a5589ff99680b64b71ea3d1fd1b5
smpletan <119008557+smpletan@users.noreply.github.com>
2025-06-15 20:40:20 -0400

    Festival/Ending implementation
    See title

--- Scripts/gameplaymenu.gd
+++ Scripts/gameplaymenu.gd
@@ -5,21 +5,14 @@ var rng = RandomNumberGenerator.new()
 var sewersVisit = ""
 var sewersFullSuccess = false
 var ending_scene = ""
-# God I really wasn't hoping to do it like this, but it's a solid fallback for a growth points system.
-#  Index : Ending
-#  0 : Indifferent (All stats below 10)
-#  1 : Horse (Speed highest at above 80, no other stat high enough)
-#  2 : Neglected (Love below 15, Evil above 40-50)
-#  3 : CEO Mindset Neutral (Diligence above 80, Smarts above 60, Evil below 30)
-#  4 : CEO Mindset Evil (Diligence above 80, Smarts above 60, Evil above 30)
-#  5 : Gym Bro (Strength + Speed above 70-80, Love above 50)
-#  6 : Firefighter (Bravery above 80, Strength above 60, Speed above 60)
-#  7 : Film Buff (Smarts above 60, Below 10 Media Literacy)
-#  8 : NL (Smarts above 60, Above 50 Media Literacy)
-#  9 : Car (Speed above 80, Driving above 50)
-# 10 : Driver (Driving at 100. Overrides any other ending.)
-# 11 : Butler (Diligence above 80, Love above 60, Smarts below 60, Bravery below 50)
-var growth_array = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+var holiday_days = [3, 7, 14, 19]
+# Holidays List
+#  0 : Big Game
+#  1 : Evil Berry
+#  2 : Hog Race
+#  3 : Film Festival
+#  4 : Spring Clean
+var holiday_complete = [false, false, false, false, false]
 
 func _ready():
 	$OuterUI/ActivityContainer.visible = false
@@ -28,18 +21,73 @@ func _ready():
 	# get_node("Background").get_node("%BackGround").get_node("AnimatedSprite2D").play("LightsOffFade_OUT")
 
 func resolve_growth():
-	growth_array[0] = (Dialogic.VAR.Stats.love < 10 && Dialogic.VAR.Stats.smarts < 10 && Dialogic.VAR.Stats.strength < 10 \
+	if (Dialogic.Var.Stats.driving >= 100) :
+		# The Driver
+		pass
+	elif (Dialogic.VAR.Stats.love < 10 && Dialogic.VAR.Stats.smarts < 10 && Dialogic.VAR.Stats.strength < 10 \
 		&& Dialogic.VAR.Stats.speed < 10 && Dialogic.VAR.Stats.bravery < 10 && Dialogic.VAR.Stats.diligence < 10 \
-		&& Dialogic.VAR.Stats.diligence < 10 && Dialogic.VAR.Stats.medialit < 10 && Dialogic.VAR.Stats.driving < 10)
-	growth_array[1] = (Dialogic.VAR.Stats.speed >= 80)
-	growth_array[2] = (Dialogic.VAR.Stats.love <= 15) + (Dialogic.VAR.Stats.evil >= 40)
-	growth_array[3] = (Dialogic.VAR.Stats.diligence >= 80) + (Dialogic.VAR.Stats.smarts >= 60) + (Dialogic.VAR.Stats.evil <= 30)
-	growth_array[4] = (Dialogic.VAR.Stats.diligence >= 80) + (Dialogic.VAR.Stats.smarts >= 60) + (Dialogic.VAR.Stats.evil > 30)
-	growth_array[5] = (Dialogic.VAR.Stats.strength >= 70) + (Dialogic.VAR.Stats.speed >= 70) + (Dialogic.VAR.Stats.love >= 50)
-	growth_array[6] = (Dialogic.VAR.Stats.bravery >= 60) + (Dialogic.VAR.Stats.strength >= 60) + (Dialogic.VAR.Stats.diligence >= 60)
-	
-	growth_array[10] = (Dialogic.VAR.Stats.driving >= 100)
-	print("Yes")
+		&& Dialogic.VAR.Stats.diligence < 10 && Dialogic.VAR.Stats.medialit < 10 && Dialogic.VAR.Stats.driving < 10) :
+		# Indifferent
+		pass
+	elif (Dialogic.VAR.Stats.love >= 80 && Dialogic.VAR.Stats.smarts >= 80 && Dialogic.VAR.Stats.strength >= 80 \
+		&& Dialogic.VAR.Stats.speed >= 80 && Dialogic.VAR.Stats.bravery >= 80 && Dialogic.VAR.Stats.diligence >= 80 \
+		&& Dialogic.VAR.Stats.diligence >= 80 && Dialogic.VAR.Stats.medialit >= 80 && Dialogic.VAR.Stats.driving >= 80) :
+		# The Writer
+		pass
+	elif (Dialogic.VAR.Stats.love < 30 && Dialogic.VAR.Stats.smarts >= 80 && Dialogic.VAR.Stats.strength < 30 \
+		&& Dialogic.VAR.Stats.speed < 30 && Dialogic.VAR.Stats.bravery < 30 && Dialogic.VAR.Stats.diligence < 30 \
+		&& Dialogic.VAR.Stats.diligence < 30 && Dialogic.VAR.Stats.medialit < 30 && Dialogic.VAR.Stats.driving < 30) :
+		# The Nerd
+		pass
+	elif (Dialogic.VAR.Stats.love < 20 && Dialogic.VAR.Stats.smarts < 20 && Dialogic.VAR.Stats.strength < 20 \
+		&& Dialogic.VAR.Stats.speed < 20 && Dialogic.VAR.Stats.bravery >= 80 && Dialogic.VAR.Stats.diligence < 20 \
+		&& Dialogic.VAR.Stats.diligence < 20 && Dialogic.VAR.Stats.medialit < 20 && Dialogic.VAR.Stats.driving < 20) :
+		# Hot Dogged
+		pass
+	elif (Dialogic.VAR.Stats.love >= 50 && Dialogic.VAR.Stats.smarts >= 50 && Dialogic.VAR.Stats.strength >= 50 \
+		&& Dialogic.VAR.Stats.speed >= 50 && Dialogic.VAR.Stats.bravery >= 50 && Dialogic.VAR.Stats.diligence >= 50 \
+		&& Dialogic.VAR.Stats.diligence >= 50) :
+		# Middle of the Road
+		pass
+	elif (Dialogic.VAR.Stats.diligence >= 90) && (Dialogic.VAR.Stats.smarts >= 90) && (Dialogic.VAR.Stats.evil >= 50) :
+		# The Wizard
+		pass
+	elif (Dialogic.VAR.Stats.diligence >= 80) && (Dialogic.VAR.Stats.smarts >= 60) && (Dialogic.VAR.Stats.evil <= 30) :
+		# CEO Normal
+		pass
+	elif (Dialogic.VAR.Stats.diligence >= 80) && (Dialogic.VAR.Stats.smarts >= 60) && (Dialogic.VAR.Stats.evil > 30) :
+		# CEO Evil
+		pass
+	elif (Dialogic.VAR.Stats.strength >= 70) && (Dialogic.VAR.Stats.speed >= 70) && (Dialogic.VAR.Stats.love >= 50) :
+		# Gym Bro
+		pass
+	elif (Dialogic.VAR.Stats.love >= 80) && (Dialogic.VAR.Stats.strength <= 30) && (Dialogic.VAR.Stats.diligence >= 40):
+		# Best in Show
+		pass
+	elif (Dialogic.VAR.Stats.love >= 80) && (Dialogic.VAR.Stats.medialit >= 50) && (Dialogic.VAR.Stats.smarts <= 50):
+		# Love to See It
+		pass
+	elif (Dialogic.VAR.Stats.bravery >= 80) && (Dialogic.VAR.Stats.love >= 80) && (Dialogic.VAR.Stats.driving >= 40):
+		# The Sully Ending
+		pass
+	elif (Dialogic.VAR.Stats.love >= 80) && (Dialogic.VAR.Stats.medialit >= 50) && (Dialogic.VAR.Stats.smarts <= 50):
+		# The Foolhardy Butler Sir
+		pass
+	elif (Dialogic.VAR.Stats.strength >= 80) && (Dialogic.VAR.Stats.love >= 80) :
+		# The Thing (of the Lake)
+		pass
+	elif (Dialogic.VAR.Stats.speed >= 80) && (Dialogic.VAR.Stats.driving >= 50) :
+		# The Car
+		pass
+	elif (Dialogic.VAR.Stats.speed >= 80) :
+		# The Horse
+		pass
+	elif (Dialogic.VAR.Stats.love <= 15) && (Dialogic.VAR.Stats.evil >= 40) :
+		# Neglected
+		pass
+	elif (Dialogic.VAR.Stats.love >= 110) :
+		# My Animal
+		pass
 	
 func check_in_range(stat: float, target: float, low_range: int, high_range: int):
 	return (stat >= target - low_range && stat <= target + high_range)
@@ -48,6 +96,26 @@ func _on_dialogic_signal(argument:String):
 	if argument == "begin_cutscene":
 		Dialogic.paused = true
 		get_node("Background").get_node("%OpeningAnimationPlayer").play("OpeningAnimation")
+	elif argument == "roll_holiday":
+		while Dialogic.VAR.next_holiday.is_empty() :
+			var roll = randi_range(0, 4)
+			if holiday_complete[roll] == false :
+				match roll:
+					0:
+						Dialogic.VAR.next_holiday = "biggame"
+						holiday_complete[0] = true
+					1:
+						Dialogic.VAR.next_holiday = "evilberry"
+						holiday_complete[1] = true
+					2:
+						Dialogic.VAR.next_holiday = "hograce"
+						holiday_complete[2] = true
+					3:
+						Dialogic.VAR.next_holiday = "filmfest"
+						holiday_complete[3] = true
+					4:
+						Dialogic.VAR.next_holiday = "springcleaning"
+						holiday_complete[4] = true
 	elif argument == "update_stats":
 		#Force a lower clamp to the Love stat.
 		if(Dialogic.VAR.Stats.love < -20):
@@ -64,9 +132,12 @@ func _on_dialogic_signal(argument:String):
 	elif argument == "city_transition":
 		get_node("Background").get_node("%EndSpriteFade").play("EndSpriteFade_IN")
 		await get_tree().create_timer(0.4).timeout
-		print("CITY")
-		#TEMPORARY BACKGROUND till we can get a city
-		get_node("Background").get_node("%Background_EndingSprite").play("Background_Playground")
+		get_node("Background").get_node("%Background_EndingSprite").play("Background_City")
+		pass
+	elif argument == "cars_transition":
+		get_node("Background").get_node("%EndSpriteFade").play("EndSpriteFade_IN")
+		await get_tree().create_timer(0.4).timeout
+		get_node("Background").get_node("%Background_EndingSprite").play("Background_Cars")
 		pass
 	elif argument == "playground_transition":
 		get_node("Background").get_node("%EndSpriteFade").play("EndSpriteFade_IN")
@@ -119,12 +190,30 @@ func _on_dialogic_signal(argument:String):
 		#get_node("Background").get_node("%ForeGroundSpriteAnimated").visible = true
 		#get_node("Background").get_node("%BackGroundReal").get_node("%BackGround").play("default")
 		pass
+	elif argument == "hotdogs_transition":
+		get_node("Background").get_node("%EndSpriteFade").play("EndSpriteFade_IN")
+		await get_tree().create_timer(0.4).timeout
+		get_node("Background").get_node("%Background_EndingSprite").play("Background_HotDogs")
+		pass
+	elif argument == "hograce_transition":
+		get_node("Background").get_node("%EndSpriteFade").play("EndSpriteFade_IN")
+		await get_tree().create_timer(0.4).timeout
+		get_node("Background").get_node("%Background_EndingSprite").play("Background_HogRace")
+		pass
+	elif argument == "evilberry_transition":
+		get_node("Background").get_node("%EndSpriteFade").play("EndSpriteFade_IN")
+		await get_tree().create_timer(0.4).timeout
+		get_node("Background").get_node("%Background_EndingSprite").play("Background_EvilBerry")
+		pass
 	elif argument == "act_workout":
 		$OuterUI/ActivityContainer.visible = true
 		$OuterUI/ActivityContainer/ActivityPlayer/ActivityAnimation.play("WorkoutBench")
 	elif argument == "act_running":
 		$OuterUI/ActivityContainer.visible = true
 		$OuterUI/ActivityContainer/ActivityPlayer/ActivityAnimation.play("RunningAround")
+	elif argument == "act_beatup":
+		$OuterUI/ActivityContainer.visible = true
+		$OuterUI/ActivityContainer/ActivityPlayer/ActivityAnimation.play("BeatUp")
 	elif argument == "act_playground":
 		$OuterUI/ActivityContainer.visible = true
 		$OuterUI/ActivityContainer/ActivityPlayer/ActivityAnimation.play("PlaygroundSwing")
@@ -144,6 +233,11 @@ func _on_dialogic_signal(argument:String):
 		$OuterUI/ActivityContainer.visible = true
 		$OuterUI/ActivityContainer/ActivityPlayer/ActivityAnimation.play("WatchTV")
 	elif argument == "act_ipad":
+		Dialogic.VAR.Stats.strength += int(rng.randi_range(-1, 1))
+		Dialogic.VAR.Stats.speed += int(rng.randi_range(-1, 1))
+		Dialogic.VAR.Stats.speed += int(rng.randi_range(-1, 1))
+		Dialogic.VAR.Stats.bravery += int(rng.randi_range(-1, 1))
+		Dialogic.VAR.Stats.medialit += int(rng.randi_range(-1, 3))
 		pass
 	elif argument == "act_pc":
 		$OuterUI/ActivityContainer.visible = true
@@ -157,6 +251,11 @@ func _on_dialogic_signal(argument:String):
 	elif argument == "end_act":
 		$OuterUI/ActivityContainer.visible = false
 		#get_node("Background").get_node("%EndSpriteFade").play("EndSpriteFade_OUT")
+	elif argument == "chance_hotdog":
+		var odds = (Dialogic.VAR.Stats.bravery / 100) * 70 + 30
+		for n in 2:
+			if( odds > rng.randi_range(0, 100) ) :
+				Dialogic.VAR.Stats.hotdog_success += 1
 	elif argument == "chance_sewers":
 		var odds = ( (Dialogic.VAR.Stats.smarts / 100) * 20 + (Dialogic.VAR.Stats.strength / 100) * 20 + 
 						(Dialogic.VAR.Stats.speed / 100) * 20 + (Dialogic.VAR.Stats.bravery / 100) * 20 + 20)
